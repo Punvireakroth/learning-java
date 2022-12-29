6a32f8d2ba1bba0553c3deb1222d62abe11c78ac
Punvireakroth <vireakrothpun@gmail.com>
2022-12-30 01:14:04 +0700

    Exercise Ten

---
+++ RUPP/Intro_Exercises/src/ExerciseTen.java
@@ -0,0 +1,12 @@
+// Group 4 M3
+
+public class ExerciseTen {
+    public static void main(String[] args) {
+        double sum = 0.0;
+
+        for (double i = 1.0; i <= 624.0; i++) { // loop 1 -> 624
+            sum += 1 / (Math.sqrt(i) + Math.sqrt(i + 1)); // denominator first difference between second 1
+        }
+        System.out.println("Total: " + sum);
+    }
+}
