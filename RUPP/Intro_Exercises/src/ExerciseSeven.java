680496a7a5cc0c768591f84ee07dc66923be9a86
Punvireakroth <vireakrothpun@gmail.com>
2022-12-30 00:55:35 +0700

    Exercise Seven

---
+++ RUPP/Intro_Exercises/src/ExerciseSeven.java
@@ -0,0 +1,19 @@
+// Group 4 M3
+
+public class ExerciseSeven {
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 100; i <= 200; i++) { // loop 100 -> 200
+            if (i % 6 == 0 || i % 5 == 0) { // find %5 or %6
+                if (!(i % 6 == 0 && i % 5 == 0)) { // find not %5 and %6
+                    System.out.print(i + " ");
+                    count += 1;
+                    if (count % 10 == 0) { // break line every 10
+                        System.out.print("\n");
+                    }
+                }
+            }
+        }
+    }
+}
