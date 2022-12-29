b58033c9b0e079a736dcc6503fba218c29d0d38e
Punvireakroth <vireakrothpun@gmail.com>
2022-12-30 01:11:27 +0700

    Exercise Nine

---
+++ RUPP/Intro_Exercises/src/ExerciseNine.java
@@ -0,0 +1,18 @@
+// Group 4 M3
+
+public class ExerciseNine {
+
+    public static void main(String[] args) {
+        int sum;
+        for (int i = 1; i < 10000; i++) { // loop 1 -> 10000
+            sum = 0; // restart
+
+            for (int j = 1; j < i; j++) { // loop each value again
+                if (i % j == 0) { // find % i between loop
+                    sum += j;
+                }
+            }
+            if (i == sum) System.out.println(i); // verify by sum
+        }
+    }
+}
