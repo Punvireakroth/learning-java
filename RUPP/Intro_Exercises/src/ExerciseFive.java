bc87743aaa4fc601f6b53681e1aad8ef4abdeeaa
Punvireakroth <vireakrothpun@gmail.com>
2022-12-30 00:33:29 +0700

    Exercise five

--- RUPP/Intro_Exercises/src/ExerciseFive.java
+++ RUPP/Intro_Exercises/src/ExerciseFive.java
@@ -4,7 +4,6 @@ public class ExerciseFive {
 
     public static void main(String[] args) {
         int n = 1;
-        if (n)
         while (Math.pow(n, 3) < 11000) { // loop when value n^3 < 11000
             n++; // update
         }
