a89b9b7ab8c19c2e5424d088960c629027e3c96b
Punvireakroth <vireakrothpun@gmail.com>
2022-12-30 00:11:15 +0700

    Added Comment

--- RUPP/Intro_Exercises/src/ExerciseOne.java
+++ RUPP/Intro_Exercises/src/ExerciseOne.java
@@ -1,9 +1,12 @@
+// team 4 M3
+
 import java.util.Scanner;
 
 class ExerciseOne {
     public static void main(String[] args) {
         int total = 0;
         Scanner input = new Scanner(System.in);
+        // add until sum value greater than or equal to 50
         while(total <= 50) {
             System.out.print("Input an Integer: ");
             int number = input.nextInt();
