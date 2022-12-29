fd6407fc644073b2e4a1879cbc7f10f8e71c1f22
Punvireakroth <vireakrothpun@gmail.com>
2022-12-30 01:06:02 +0700

    Exercise Seven

---
+++ RUPP/Intro_Exercises/src/ExerciseEight.java
@@ -0,0 +1,14 @@
+// Team 4 M3
+
+public class ExerciseEight {
+
+    public static void main(String[] args) {
+        double sum = 0.0; // for + double
+
+        for (double i = 1; i <= 97; i += 2) { // loop numerator
+            sum += i / (i + 2); // denominator difference between numerator 2
+        }
+
+        System.out.print("sum: " + sum);
+    }
+}
