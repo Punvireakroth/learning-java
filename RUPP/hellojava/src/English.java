27b03a40e6f478af0ca390437f1cae1af9c0ae9e
Punvireakroth <vireakrothpun@gmail.com>
2022-12-29 22:02:42 +0700

    initial commit

---
+++ RUPP/hellojava/src/English.java
@@ -0,0 +1,7 @@
+public class English {
+    public static void main(String[] args){
+        final int dayInWeek = 7;
+        System.out.println(dayInWeek);
+
+    }
+}
---
+++ RUPP/hellojava/src/Welcome.java
@@ -0,0 +1,8 @@
+class Welcome {
+    public static void main(String[] args) // main method
+    {
+        System.out.println("Hello VireakRoth");
+    }
+}
+
+// we do not need to import java.lang because it already available whe
