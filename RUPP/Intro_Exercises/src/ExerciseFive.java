3323e814b7672804bd0a430ec5f572f93862b666
Punvireakroth <vireakrothpun@gmail.com>
2023-01-05 23:00:53 +0700

    Finish Exercise 5

--- RUPP/Intro_Exercises/src/ExerciseFive.java
+++ RUPP/Intro_Exercises/src/ExerciseFive.java
@@ -3,17 +3,13 @@
 public class ExerciseFive {
 
     public static void main(String[] args) {
-        int n = 1;
+        int n = 0;
         while (Math.pow(n, 3) < 11000) { // loop when value n^3 < 11000
-            n++; // update
+            n += 2;
+            System.out.print(n + " ");
         }
-
-        if (n % 2 != 0) { // covert odd to even
-            n -= 1;
-        }
-
         System.out.print(
-                "The largest even integer n is " + n + "."
+                "The largest even integer n is " + (n - 2) + "."
         );
     }
 }
