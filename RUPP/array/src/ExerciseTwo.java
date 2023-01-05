54fe8f9b355ff943c92612a2ff0421949b1afac4
Punvireakroth <vireakrothpun@gmail.com>
2023-01-05 22:59:49 +0700

    Finish Exercise 2

---
+++ RUPP/array/src/ExerciseTwo.java
@@ -0,0 +1,31 @@
+
+
+import java.util.Scanner;
+
+public class ExerciseTwo {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        String message, newMessage = "";
+
+        System.out.print("input: ");
+        message = input.nextLine();
+
+        // Set first char as Capital letter
+        newMessage += Character.toUpperCase(message.charAt(0));
+
+        // Check if the last string is space capital the current character
+        for (int i = 1; i < message.length(); i++) {
+            if (message.charAt(i - 1) == ' ') {
+                newMessage += Character.toUpperCase(message.charAt(i));
+            } else {
+                newMessage += message.charAt(i);
+            }
+        }
+
+        System.out.println("new message: " + newMessage);
+        input.close();
+    }
+}
